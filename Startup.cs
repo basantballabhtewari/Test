ab1abf3b0269be955381cd6841db93563c0d1df8
basantballabhtewari <82369997+basantballabhtewari@users.noreply.github.com>
2021-04-16 10:07:07 +0530

    test
    odjfspothjr

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace Test
 
         public IConfiguration Configuration { get; }
 
-        //  aaa ;askjrgherk;btrw'oThis method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
